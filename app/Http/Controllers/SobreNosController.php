f1a11a1c6de8c581facb3f83d50a437ceec4c217
HidraForce <vierabruno4@gmail.com>
2023-01-26 20:08:49 -0300

    26/01

---
+++ app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    //
+    public function SobreNos()
+    {
+        # code...
+        return view('site.sobrenos');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/sobre-nos', function () {
-    return view('welcome');
-});
-Route::get('/contato', function () {
-    return view('welcome');
-});
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/sobre-nos', 'SobreNosController@SobreNos')->name('site.sobrenos');
+
+Route::get('/login', function () {
+    return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    }
+    )->name('app.clientes');
+    
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';})->name('app.fornecedores');
+        
+    Route::get('/produtos', function () {
+        return 'Produtos';})->name('app.produtos');
+    
+
+});    
